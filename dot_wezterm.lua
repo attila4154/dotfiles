c2ad9bb4173c10649474cb08034aaf7a86a910fe
Danila Makulov <danila.makulov@pipedrive.com>
2026-01-17 22:33:02 +0100

    wezterm: add missing bg opacity

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
 config.macos_window_background_blur = 10
+config.window_background_opacity = 0.93
 
 config.font_size = 16
 
