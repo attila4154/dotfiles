68198b01fa9914805a4871fa387c2af525940a69
Danila Makulov <attila415422@gmail.com>
2026-01-17 17:57:31 +0100

    testing updates

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Macchiato"
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
