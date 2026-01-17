d02819239205e291a011bf2ddae02bd2e27a6915
Danila Makulov <danila.makulov@pipedrive.com>
2026-01-17 18:21:02 +0100

    updating wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,14 +1,39 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
-config.color_scheme = "Catppuccin Macchiato"
-config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
+
+config.font_size = 16
+
+-- config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "nord"
+-- config.color_scheme = "Kanagawa (Gogh)"
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
+	-- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act.SendKey({ key = "f", mods = "ALT" }),
+	},
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-1) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(1) },
+	{ key = "UpArrow", mods = "CTRL|SHIFT", action = act.ScrollByLine(-5) },
+	{ key = "DownArrow", mods = "CTRL|SHIFT", action = act.ScrollByLine(5) },
 	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 }
 
