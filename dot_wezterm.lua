f3fd9888c4a587d60a5c4f40c5c82fbb925da99a
Danila Makulov <attila415422@gmail.com>
2026-01-17 17:48:03 +0100

    add wezterm.lua config

---
+++ dot_wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- config.window_decorations = "RESIZE"
+config.color_scheme = "Catppuccin Macchiato"
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+config.use_fancy_tab_bar = false
+
+config.keys = {
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
+}
+
+return config
